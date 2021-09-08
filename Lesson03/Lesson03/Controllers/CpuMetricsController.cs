154bf66dde4ca7c1172c3c780aaa570b17a15879
vadim2071 <vadim207138@gmail.com>
2021-09-08 23:09:07 +0300

    Update

---
+++ Lesson03/Lesson03/Controllers/CpuMetricsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SQLite;
+
+namespace MetricsAgent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CpuMetricsController : ControllerBase
+    {
+        [HttpGet("sql-test")]
+        public IActionResult TryToSqlLite()
+        {
+            string cs = "Data Source=:memory:";
+            string stm = "SELECT SQLITE_VERSION()";
+
+
+            using (var con = new SQLiteConnection(cs))
+            {
+                con.Open();
+
+                using var cmd = new SQLiteCommand(stm, con);
+                string version = cmd.ExecuteScalar().ToString();
+
+                return Ok(version);
+            }
+        }
+    }
+}
