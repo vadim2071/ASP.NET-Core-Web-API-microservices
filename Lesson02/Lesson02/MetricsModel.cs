fee475e7cd4ca9979a628419e0bdc20ffc813e63
Vadim2071 <vadim207138@gmail.com>
2021-09-08 15:51:23 +0300

    End Lesson02 update v.2

---
+++ Lesson02/Lesson02/MetricsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    public class MetricsModel
+    {
+        public int Value { get; set; }
+        public DateTime Time { get; set; }
+
+        public MetricsModel()
+        {
+
+        }
+
+        public MetricsModel(int value, DateTime dateTime)
+        {
+            Value = value;
+            Time = dateTime;
+        }
+    }
+}
